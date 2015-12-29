a8c27deea9da287b466b24d64d6ac9ecdf3b5f7f
unholydeath785 <evan@coulson-orellana.com>
2015-12-28 23:28:46 -0500

    finished up basic styling need footer and parts of results as well as finishing the grayscale portion of app

--- Assets/Scripts/JS/app.js
+++ Assets/Scripts/JS/app.js
@@ -5,8 +5,8 @@ var lithiumApp = (function ($) {
   //functions
   var carousel = function () {
     var loop = setInterval(next,10000);
-    $(".header").css("background-size", "cover");
-    $(".header").css("background-image", "url(Assets/Images/slide"+currentSlide+".jpg)");
+    $(".header .container").css("background-size", "cover");
+    $(".header .container").css("background-image", "url(Assets/Images/slide"+currentSlide+".jpg)");
 
     function next() {
       currentSlide += 1;
@@ -17,8 +17,32 @@ var lithiumApp = (function ($) {
     }
   }
 
+  var showSections = function() {
+    $(window).scroll(function () {
+      var scrollOffsetY = window.pageYOffset;
+      var scrollChange = 50;
+      if (scrollOffsetY > scrollChange) {
+        $('.about .container').fadeIn(3500);
+        $('.header').css("height","650");
+        $('.about').css("transition","transform 2s");
+        $('.about').css("transform","rotateY(0deg)");
+        $('.result').css("transition","transform 2s");
+        $('.result').css("transform","rotateY(0deg)");
+        $('.result .container').fadeIn(3500);
+        $('.original-image').fadeIn(3500);
+      }
+      if (scrollOffsetY > scrollChange + 100) {
+        $('.container').fadeIn(3500);
+        $('.container').css("display","block");
+        $('.submit').css("transition","transform 2s");
+        $('.submit').css("transform","rotateY(0deg)");
+      }
+    });
+  }
+
   var ignition = function () {
     carousel();
+    showSections();
   }
 
   //return
@@ -31,3 +55,47 @@ var lithiumApp = (function ($) {
 $(document).ready(function () {
   lithiumApp.start();
 });
+
+function previewImage (input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var imgName = $('body').find('#preview').attr('name');
+      $('.file-name').text(imgName);
+      $('#preview').attr('src',e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function grayScaleImage (image, bPlaceImage) {
+  var myCanvas = document.getElementById('greyscale');
+  var myCanvasContext = myCanvas.getContext("2d");
+  var imageWidth = image.width;
+  var imageHeight = image.height;
+
+  myCanvas.width = image.width;
+  myCanvas.height = image.height;
+  myCanvasContext.drawImage(image,0,0);
+
+  var imageData = myCanvasContext.getImageData(0,0,imageWidth,imageHeight);
+
+  for (var j = 0; j < imageData.height; j++) {
+    for (var i = 0; i < imageData.width; i++) {
+      var index = (i*4) * imageData.width + (j*4);
+      var red = imageData.data[index];
+      var green = imageData.data[index+1];
+      var blue = imageData.data[index+2];
+      var alpha = imageData.data[index+3];
+      var average = (red+green+blue)/3;
+      imageData.data[index] = average;
+      imageData.data[index+1] = average;
+      imageData.data[index+2] = average;
+      imageData.data[index+3] = alpha;
+    }
+  }
+  if (bPlaceImage) {
+    
+  }
+  return myCanvas.toDataURL();
+}
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
+$file_name = "No File Choosen";
+$imagePath = "Assets/Images/placeholder.jpg";
+if(isset($_FILES['image'])) {
+  //if ((@$_FILES['image']['type']) == 'image/jpg' || (@$_FILES['image']['type']) == 'image/png') {
+    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    $dir_name = substr(str_shuffle($chars),0,15);
+    mkdir("Assets/Images/toGreyscale/$dir_name/");
+    move_uploaded_file($_FILES['image']['tmp_name'], "Assets/Images/toGreyscale/$dir_name/".$_FILES['image']['name']);
+    $imagePath = "Assets/Images/toGreyscale/$dir_name/".$_FILES['image']['name'];
+  //}
+}
  ?>
 <!DOCTYPE html>
 <html>
@@ -20,17 +30,46 @@
       </div>
     </header>
 
-    <section id="about" class="about">
-      
-    </section>
+    <div class="col-1">
+      <section id="about" class="about">
+        <div class="container">
+          <h3 class="section-header">About</h3>
+          <p class="about-text">
+            Lithium.io is a simple application which grayscales an image which has been submited through php then using canvas the image is greycaled and compared.
+          </p>
+          <h3 class="section-header">How To Use</h3>
+          <p class="about-text">
+            In order to use this webapp you must download an image to your computer than submit it. Second wait then it will be uploaded and greyscaled.
+          </p>
+        </div>
+      </section>
 
-    <section id="submit" class="submit">
+      <section id="submit" class="submit">
+        <div class="container">
+          <h3 class="section-header">Submit Photo</h3>
+          <form class="image-submit" action="index.php" method="post" enctype='multipart/form-data'>
+            <label class="custom-upload" for="file-submit"><span class="file-icon"></span>Choose A File</label>
+            <input id="file-submit" type="file" name="image" value="Submit Image" onchange="previewImage(this);"><br><br>
+            <img class="placeholder-img" id="preview" src="Assets/Images/placeholder.jpg" alt="placement img" /><br>
+            <input class="send" type="submit" name="submit" value="Submit">
+          </form>
+        </div>
+      </section>
+    </div>
 
-    </section>
-
-    <section id="result" class="result">
-
-    </section>
+    <div class="col-2">
+      <section id="result" class="result">
+        <div class="container">
+          <h3 class="section-header">Result</h3>
+          <div class="original">
+            <img class="original-image" src=<?php if ($imagePath != "") { print($imagePath); }  ?> onload="grayScaleImage(this,true);" alt="placement img" />
+          </div>
+          <div class="result">
+            <canvas id="greyscale" width="300" height="300"></canvas>
+          </div>
+        </div>
+      </section>
+    </div>
 
     <footer id="bot" class="footer">
 
